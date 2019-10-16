2e5fbb4f63dde0162807b0fa31453fb0df1a0bfb
eshikvtumane <eshikvtumanebest@gmail.com>
2019-10-16 17:13:25 +1000

    Change selector

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ class TestShop():
     def test_check_button_add_item(self, browser):
         browser.get('http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/')
 
-        button = browser.find_element_by_css_selector('#add_to_basket_form>button')
+        button = browser.find_element_by_css_selector('.btn-add-to-basket')
 
         # time.sleep(30)
 
